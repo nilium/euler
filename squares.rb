3e1184407a71a0c912e0f60994b9c0206fe8e343
Noel Cower <me@spifftastic.net>
2012-07-05 16:31:46 -0700

    Shrink the code a tiny bit.

--- squares.rb
+++ squares.rb
@@ -1,5 +1,7 @@
 #!/usr/bin/env ruby
-r = Range.new 1, 100
+
+# Well, that was easier.
+r = 1..100
 sum_of_squares = r.reduce(0) { |m, i| m + (i ** 2) }
 square_of_sum = (r.reduce(0) { |m, i| m + i }) ** 2
 
