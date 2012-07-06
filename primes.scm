8bd39db741fd9f286fa911a3564081f411955854
Noel Cower <me@spifftastic.net>
2012-07-05 18:06:13 -0700

    Remove 'prime?'.
    Just wraps 'factors?', so there's not much point to keeping it.

--- primes.scm
+++ primes.scm
@@ -1,17 +1,13 @@
 (define target 10001)
 
 ;; returns true if any of the factors in facs are a factor of num
-(define (factors? facs num)
-  (if (not (null? facs))
-      (let loop ((facs-iter facs))
-       (cond
-        ((null? facs-iter) #f)
-        ((= 0 (modulo num (car facs-iter))) #t)
-        (else (loop (cdr facs-iter)))))))
-
-;; Tests the number against other primes
-(define (prime? num divisors)
-  (not (factors? divisors num)))
+(define factors?
+ (lambda (facs num)
+  (let loop ((facs-iter facs))
+   (cond
+    ((null? facs-iter) #f)
+    ((= 0 (modulo num (car facs-iter))) #t)
+    (else (loop (cdr facs-iter)))))))
 
 (begin
   (display (car
@@ -19,7 +15,7 @@
               (primes '(2))
               (n 3))
      (if (< prime-count target)
-       (let ((is-prime (prime? n primes)))
+       (let ((is-prime (not (factors? primes n))))
        (begin
          ;(if is-prime (pp (list (+ 1 prime-count) n)))
          (loop (if is-prime
